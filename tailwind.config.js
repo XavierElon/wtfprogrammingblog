3c12cafe5a916a1870c02f2a47c96ec908642d38
Xavier <xavierh93@hotmail.com>
2024-11-20 16:25:13 -0800

    update packages; working on getting tailwinds to work

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,11 @@ module.exports = {
     './layouts/**/*.html',
     './content/**/*.md',
     './content/**/*.html',
-    './themes/**/layouts/**/*.html' // if you're using a theme
+    './themes/**/layouts/**/*.html', // if you're using a theme
   ],
   theme: {
-    extend: {}
+    extend: {},
   },
-  plugins: []
-}
+  plugins: [],
+  purge: ['./src/**/*.{js,jsx,ts,tsx,html}', './public/index.html'],
+};
